ddcbe432e9572682b2fa1cc183421a76a1b9da4d
Robin Pohlman <robin.pohlman@gmail.com>
2024-12-10 08:58:45 +0100

    feat: update Prisma dependencies to version 6.0.1, add user profile update route, and clean up auth routes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,13 +1,12 @@
-
-import e, { Request, Response } from 'express';
+import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export const register = async (req: Request, res: Response) => {
-  const { name, username, email, password,  } = req.body;
+  const { name, username, email, password } = req.body;
 
   try {
     // Validate input
@@ -30,10 +29,10 @@ export const register = async (req: Request, res: Response) => {
     const newUser = await prisma.user.create({
       data: {
         name: name,
-        username: username, // Adjust as per your User model
-        email: email, // Assuming email is used as username
+        username: username,
+        email: email,
         password: hashedPassword,
-        authentication: 'EmailPassword', // Add the correct authentication method here
+        authentication: "EmailPassword"
       },
     });
 
@@ -155,4 +154,41 @@ export const deleteUser = async (req: Request, res: Response) => {
 };
 
 
+export const updateUser = async (req: Request, res: Response) => {
+  const { username } = req.params;
+  const { name, email, isActive, authentication } = req.body as Prisma.UserUpdateInput;
+
+  // Partial object to store the updated data
+  const updateData: Prisma.UserUpdateInput = {};
+
+  if (name !== undefined) updateData.name = name;
+  if (email !== undefined) updateData.email = email;
+  if (isActive !== undefined) updateData.isActive = isActive;
+  if (authentication !== undefined) updateData.authentication = authentication;
+
+  try {
+    console.log(updateData);
+    // Find and update the user by username
+    const updatedUser = await prisma.user.update({
+      where: { username: username },
+      data: updateData,
+    });
+
+    res.json({
+      message: 'User updated successfully',
+      user: updateData,
+    });
+  } catch (error) {
+    console.error(error);
+
+    // Type assertion to narrow down the error type
+    const prismaError = error as { code?: string };
+
+    if (prismaError.code === 'P2025') { // Prisma error code for record not found
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,20 +1,19 @@
 import { Router } from 'express';
-import { getAllUserProfiles, getUserByUsername, deleteUser, register } from '../controllers/userController'; 
+import { getAllUserProfiles, getUserByUsername, deleteUser, register, updateUser } from '../controllers/userController'; 
 import authenticateToken from '../lib/middleware/authentication';
 import registerLimiter from '../lib/middleware/registerRateLimit';
 import { userValidationRules } from '../lib/validation/userValidation';
 import validateRequest from '../lib/middleware/validator';
+
 
 // Register routes
 const router = Router();
 
 // Apply the rate limiter and validation before the register handler
 router.post('/register', registerLimiter, userValidationRules, validateRequest, register);
-
+router.put('/profile/:username', authenticateToken, validateRequest, updateUser);
 router.get('/profiles/', authenticateToken, getAllUserProfiles);
-
 router.get('/profile/:username', authenticateToken, getUserByUsername);
-
-router.delete('/profile/:username', authenticateToken, deleteUser); // Define the route for deleting a user
+router.delete('/profile/:username', authenticateToken, deleteUser); 
 
 export default router;
